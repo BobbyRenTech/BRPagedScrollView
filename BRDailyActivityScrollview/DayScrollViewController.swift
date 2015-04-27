933fff086ffc66bf4224e3954648c1ad36e9fb8c
mitrenegade <BobboMac@Bobbys-MacBook-Pro.local>
2015-04-27 15:15:29 -0400

    programmatically add days to scrollview

--- BRDailyActivityScrollview/DayScrollViewController.swift
+++ BRDailyActivityScrollview/DayScrollViewController.swift
@@ -9,11 +9,33 @@
 import UIKit
 
 class DayScrollViewController: UIViewController {
+    @IBOutlet weak var constraintContentWidth: NSLayoutConstraint!
+    @IBOutlet weak var constraintContentHeight: NSLayoutConstraint!
 
+    @IBOutlet weak var labelText: UILabel!
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var contentView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        self.view.backgroundColor = self.randomColor()
+
+        /*
+        let appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
+        let width = appDelegate.window!.frame.size.width
+        let height = appDelegate.window!.frame.size.height
+        
+        self.constraintContentWidth.constant = width;
+        self.constraintContentHeight.constant = height;
+
+        self.contentView.setNeedsUpdateConstraints()
+        self.contentView.layoutIfNeeded()
+*/
+//        println("initial scroll: \(scrollView.frame.origin.x) \(scrollView.frame.origin.y) \(scrollView.frame.size.width) \(scrollView.frame.size.height)")
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,7 +43,14 @@ class DayScrollViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
+    override func viewDidAppear(animated: Bool) {
+        println("view: \(self.view.frame.origin.x) \(self.view.frame.origin.y) \(self.view.frame.size.width) \(self.view.frame.size.height)")
+        /*
+        println("appeared scroll: \(scrollView.frame.origin.x) \(scrollView.frame.origin.y) \(scrollView.frame.size.width) \(scrollView.frame.size.height)")
+        println("content: \(contentView.frame.origin.x) \(contentView.frame.origin.y) \(contentView.frame.size.width) \(contentView.frame.size.height)")
+        println("cell: \(labelText.frame.origin.x) \(labelText.frame.origin.y) \(labelText.frame.size.width) \(labelText.frame.size.height)")
+*/
+    }
     /*
     // MARK: - Navigation
 
@@ -32,4 +61,10 @@ class DayScrollViewController: UIViewController {
     }
     */
 
+    func randomColor() -> UIColor {
+//        return UIColor.blackColor()
+        let colors = [UIColor.redColor(), UIColor.blueColor(), UIColor.yellowColor(), UIColor.greenColor(), UIColor.brownColor()] as Array
+        let index = arc4random_uniform(UInt32(colors.count))
+        return colors[Int(index)]
+    }
 }
--- BRDailyActivityScrollview/HorizontalTimelineController.swift
+++ BRDailyActivityScrollview/HorizontalTimelineController.swift
@@ -10,18 +10,47 @@ import UIKit
 
 class HorizontalTimelineController: UIViewController {
 
+    @IBOutlet weak var scrollview : UIScrollView!
+//    @IBOutlet weak var contentView : UIView!
+    
+    @IBOutlet weak var constraintContentWidth: NSLayoutConstraint!
+    @IBOutlet weak var constraintContentHeight: NSLayoutConstraint!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        
-        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    override func viewDidAppear(animated: Bool) {
+        self.populateDays()
+    }
 
+    func populateDays() {
+        let appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
+        let width = appDelegate.window!.frame.size.width
+        let height = appDelegate.window!.frame.size.height
 
+        let days = arc4random_uniform(5) + 3
+        for index in 0...days-1 {
+            let i = CGFloat(index)
+            var frame = appDelegate.window!.frame as CGRect
+            frame.origin.x = i * width
+            
+            let dayController = storyboard!.instantiateViewControllerWithIdentifier("DayScrollViewController") as! DayScrollViewController
+            self.addChildViewController(dayController)
+            dayController.view.frame = frame
+
+            self.scrollview.insertSubview(dayController.view, atIndex: 0)
+            dayController.didMoveToParentViewController(self)
+        }
+        
+        self.scrollview.contentSize = CGSizeMake(CGFloat(days)*width + 50, height)
+        
+    }
 }
 
